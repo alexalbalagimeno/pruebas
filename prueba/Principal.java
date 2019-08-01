7cc6501687020fbef98e2652ae66c20388565de8
alexalbalagimenogmail <alexalbalagimeno@gmail.com>
2019-08-01 15:54:59 +0200

    Commit1

--- prueba/Principal.java
+++ prueba/Principal.java
@@ -5,12 +5,6 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Commit1");
 		System.out.println("Commit2");
-		System.out.println("Commit3");
-		System.out.println("Commit4");
-		System.out.println("Commit5");
-		System.out.println("Commit6");
-		System.out.println("Commit7");
-		System.out.println("Commit8");
 	}
 
 }
