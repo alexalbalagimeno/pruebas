53a0d6e9581a284b6e9c6fc7cca395b97289f39c
alexalbalagimenogmail <alexalbalagimeno@gmail.com>
2019-08-02 07:57:23 +0200

    Commit5 master2

--- prueba/Principal.java
+++ prueba/Principal.java
@@ -7,7 +7,7 @@ public class Principal {
 		System.out.println("Commit2");
 		System.out.println("Commit3 master2");
 		System.out.println("Commit4 master2");
-		
+		System.out.println("Commit5 master2");
 	}
 
 }
